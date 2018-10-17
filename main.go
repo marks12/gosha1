37249aabbafdecefc437565040e79a9278cb0c1d
TSV <tsv@serveon.ru>
2018-10-17 22:52:21 +0700

    run

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
     "fmt"
-    "os"
     "gosha/cmd"
 )
 
 func main() {
 
-    fmt.Println("Hello, this is Microservice Remote Procedure Call application program interface. " +
-        "Use https://msrpc document for more information")
+    fmt.Println("Hello, this is Go Shell for help you write a code")
+
+    cmd.Run()
 }
 
 
