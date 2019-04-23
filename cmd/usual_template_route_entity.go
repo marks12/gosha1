66d7312c3ac011cc3e4ceb4821ffbe093e05019a
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2019-04-23 19:45:22 +0700

    bf

--- cmd/usual_template_route_entity.go
+++ cmd/usual_template_route_entity.go
@@ -1,11 +1,11 @@
 package cmd
 
 const usualRouteEntity = `[ {Entity} ]
-	router.HandleFunc("/api/v1/{entity}/{id}",  webapp.{Entity}Read).Methods("GET")
-	router.HandleFunc("/api/v1/{entity}",       webapp.{Entity}Find).Methods("GET")
-	router.HandleFunc("/api/v1/{entity}",       webapp.{Entity}Create).Methods("POST")
-	router.HandleFunc("/api/v1/{entity}",       webapp.{Entity}Update).Methods("PUT")
-	router.HandleFunc("/api/v1/{entity}/{id}",  webapp.{Entity}Delete).Methods("DELETE")
+	router.HandleFunc("/api/v1/{entity}/{id}",      webapp.{Entity}Read).Methods("GET")
+	router.HandleFunc("/api/v1/{entity}",           webapp.{Entity}Find).Methods("GET")
+	router.HandleFunc("/api/v1/{entity}",           webapp.{Entity}Create).Methods("POST")
+	router.HandleFunc("/api/v1/{entity}/{id}",      webapp.{Entity}Update).Methods("PUT")
+	router.HandleFunc("/api/v1/{entity}/{id}",      webapp.{Entity}Delete).Methods("DELETE")
 
     //router-generator here dont touch this line`
 
