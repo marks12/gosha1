7b7372188806d5d8587b8a7c316e7cd476735601
tsv <tsv@serveon.ru>
2019-07-31 01:39:52 +0700

    front

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <TheLayout id="app">
+        <template #navigation>
+            <MainMenu></MainMenu>
+        </template>
+        <template #content>
+            <router-view #content/>
+        </template>
+    </TheLayout>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
+<script>
+    import TheLayout from "swtui/src/components/TheLayout";
+    import MainMenu from "./components/MainMenu";
+    export default {
+        components: {MainMenu, TheLayout}
+    }
+</script>
