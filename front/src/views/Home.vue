1e469fef57701aea3c1f1b2714be1f7649911fdf
tsv <tsv@serveon.ru>
2019-07-31 23:34:09 +0700

    changes

---
+++ front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <WorkSpace>
+    <template #content>
+      Some content
+    </template>
+  </WorkSpace>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import WorkSpace from "swtui/src/components/WorkSpace";
+export default {
+  name: 'home',
+  components: {
+      WorkSpace,
+  }
+}
+</script>
