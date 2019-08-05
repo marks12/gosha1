a8206932dfc10f3a05e3aab702153a98e00e26b6
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2019-08-05 18:06:03 +0700

    global sort add check fields

--- cmd/usual_templates_entity_logic.go
+++ cmd/usual_templates_entity_logic.go
@@ -32,7 +32,12 @@ func {Entity}Find(filter types.{Entity}Filter)  (result []types.{Entity}, totalR
     // order global criteria
     if len(filter.Order) > 0  {
         for index, field := range filter.Order {
-            criteria = criteria.Order(field + " " + filter.OrderDirection[index])
+             if core.Db.NewScope(&dbmodels.{Entity}{}).HasColumn(field) {
+                criteria = criteria.Order(` + `"` + "`" + `"` + `field` + `"` + "`" + `"` + ` + " " + filter.OrderDirection[index])
+            } else {
+                err = errors.New("Ordering by unknown field " + field)
+                return
+            }
         }
     }
 
