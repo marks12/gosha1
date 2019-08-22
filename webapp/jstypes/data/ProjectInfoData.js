a1f8308ca3be2f2d505cbbf35197b7dac1d88985
tsv <tsv@serveon.ru>
2019-08-23 00:20:05 +0700

    BuBu library

---
+++ webapp/jstypes/data/ProjectInfoData.js
@@ -0,0 +1,28 @@
+
+	import {ProjectInfo, ProjectInfoFilter} from '../apiModel';
+
+let projectInfoData = function () {
+    return {
+        projectInfoFilter: new ProjectInfoFilter(),
+        panel: {
+            type: '',
+            show: false,
+            create: 'create',
+            edit: 'edit'
+        },
+        panelHeaderCreate: 'Создать',
+        panelHeaderEdit: 'Изменить',
+        panelSubmitButtonTextCreate: 'Создать',
+        panelSubmitButtonTextEdit: 'Изменить',
+        currentProjectInfoItem: {
+            item: new ProjectInfo(),
+            hasChange: false,
+            isSelected: false,
+            canDelete: false,
+            showDeleteConfirmation: false,
+        },
+    }
+}();
+
+export default projectInfoData;
+
---
+++ webapp/jstypes/store/SettingFilter.js
@@ -0,0 +1,149 @@
+
+import {SettingFilter} from "../apiModel";
+import api from "../api";
+import {findItemIndex} from "../common";
+
+let findUrl = "/api/v1/settingFilter";
+let readUrl = "/api/v1/settingFilter/"; // + id
+let createUrl = "/api/v1/settingFilter";
+let updateUrl = "/api/v1/settingFilter/"; // + id
+let deleteUrl = "/api/v1/settingFilter/"; // + id
+let findOrCreateUrl = "/api/v1/settingFilter"; // + id
+
+const settingFilter = {
+    actions: {
+        createSettingFilter(context, {data, filter, header}) {
+
+            return api.create(createUrl, data, filter, header)
+                .then(function(response) {
+
+                    context.commit("setSettingFilter", response.Model);
+
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        deleteSettingFilter(context, {id, header}) {
+
+            return api.remove(deleteUrl + id, header)
+                .then(function(response) {
+                    context.commit("clearSettingFilter");
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        findSettingFilter(context, {filter, header}) {
+
+            return api.find(findUrl, filter, header)
+                .then(function(response) {
+
+                    context.commit("setSettingFilter__List", response.List);
+
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        loadSettingFilter(context, {id, filter, header}) {
+
+            return api.find(readUrl + id, filter, header)
+                .then(function(response) {
+
+                    context.commit("setSettingFilter", response.Model);
+
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        updateSettingFilter(context, {id, data, filter, header}) {
+
+            return api.update(updateUrl + id, data, filter, header)
+                .then(function(response) {
+
+                    context.commit("setSettingFilter", response.Model);
+
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        findOrCreateSettingFilter(context, {id, data, filter, header}) {
+
+            return api.update(findOrCreateUrl, data, filter, header)
+                .then(function(response) {
+
+                    context.commit("setSettingFilter", response.Model);
+
+                    return response;
+                })
+                .catch(function(err) {
+                    return err;
+                });
+        },
+        clearListSettingFilter(context) {
+            context.commit("clearListSettingFilter");
+        },
+    },
+    getters: {
+        getSettingFilter: (state) => {
+            return state.SettingFilter;
+        },
+        getListSettingFilter: (state) => {
+            return state.SettingFilter__List;
+        },
+    },
+    mutations: {
+        setSettingFilter(state, data) {
+            state.SettingFilter = data;
+        },
+        setSettingFilter__List(state, data) {
+            state.SettingFilter__List = data;
+        },
+        clearSettingFilter(state) {
+            state.SettingFilter = new SettingFilter();
+        },
+        clearListSettingFilter(state) {
+            state.SettingFilter__List = [];
+        },
+		updateSettingFilterById(state, data) {
+    		let index = findItemIndex(state.SettingFilter__List, function(item) {
+	        	return item.Id === data.Id;
+	    	});
+	    
+	    	if (index || index === 0) {
+		        state.SettingFilter__List.splice(index, 1, data);
+    		}
+		},
+		deleteSettingFilterFromList(state, id) {
+		    let index = findItemIndex(state.SettingFilter__List, function(item) {
+		        return item.Id === id;
+		    });
+		    
+		    if (index || index === 0) {
+		        state.SettingFilter__List.splice(index, 1);
+		    }
+		},
+		addSettingFilterItemToList(state, item) {
+
+			if (state.SettingFilter__List === null) {
+				state.SettingFilter__List = [];
+			}
+
+		    state.SettingFilter__List.push(item);
+		},
+    },
+    state: {
+        SettingFilter: new SettingFilter(),
+        SettingFilter__List: [],
+    },
+};
+
+export default settingFilter;
