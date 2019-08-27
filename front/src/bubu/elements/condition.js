071b2a93d0a512394042044cfdb800943721778a
tsv <tsv@serveon.ru>
2019-08-27 22:46:04 +0700

    move elements in bubu

--- front/src/bubu/elements/condition.js
+++ front/src/bubu/elements/condition.js
@@ -4,6 +4,7 @@ import {TYPES} from "../constants";
 import Coordinates from "../attributes/coordinates";
 import Id from "../attributes/id";
 import Draw from "../actions/draw";
+import Size from "../attributes/size";
 
 function Condition(config) {
 
@@ -12,6 +13,10 @@ function Condition(config) {
     Names.apply(this, arguments);
     Types.apply(this, arguments);
     this.SetType(TYPES.CONDITION);
+
+    Size.apply(this, arguments);
+    this.SetSize(200, 200);
+
 
     Draw.apply(this, arguments);
 
