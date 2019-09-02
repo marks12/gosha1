8ea1bde65626e7a25efad97249ef3272c7bebdf5
tsv <tsv@serveon.ru>
2019-09-03 00:30:16 +0700

    works

--- front/src/bubu/elements/condition.js
+++ front/src/bubu/elements/condition.js
@@ -1,24 +1,13 @@
-import Names from "../attributes/names";
 import Types from "../attributes/types";
 import {TYPES} from "../constants";
-import Coordinates from "../attributes/coordinates";
-import Id from "../attributes/id";
-import Draw from "../actions/draw";
-import Size from "../attributes/size";
-import Move from "../actions/move";
+import BasicSet from "../attributes/basic-set";
 
 function Condition(config) {
 
-    Id.apply(this, arguments);
-    Coordinates.apply(this, arguments);
-    Names.apply(this, arguments);
+    BasicSet.apply(this, arguments);
+
     Types.apply(this, arguments);
     this.SetType(TYPES.CONDITION);
-
-    Size.apply(this, arguments);
-
-    Draw.apply(this, arguments);
-    Move.apply(this, arguments);
 
     return this;
 }
---
+++ front/src/bubu/elements/divider.js
@@ -0,0 +1,21 @@
+import Id from "../attributes/id";
+import Types from "../attributes/types";
+import {TYPES} from "../constants";
+import Coordinates from "../attributes/coordinates";
+import Draw from "../actions/draw";
+import Size from "../attributes/size";
+import Color from "../attributes/color";
+import Move from "../actions/move";
+import BasicSet from "../attributes/basic-set";
+
+function Divider(config) {
+
+
+    BasicSet.apply(this, arguments);
+    Types.apply(this, arguments);
+    this.SetType(TYPES.DIVIDER);
+
+    return this;
+}
+
+export default Divider;
