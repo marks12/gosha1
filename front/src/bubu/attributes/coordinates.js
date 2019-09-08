0451b23932cdec4aa93ddbb43e18d9fb111452a2
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2019-09-08 18:29:40 +0700

    group select almost works

--- front/src/bubu/attributes/coordinates.js
+++ front/src/bubu/attributes/coordinates.js
@@ -3,13 +3,24 @@ function Coordinates(config) {
     let X = config && config.Coords && typeof config.Coords.X === "number" ? config.Coords.X : 0;
     let Y = config && config.Coords && typeof config.Coords.Y === "number" ? config.Coords.Y : 0;
 
+    let PreviousX = 0;
+    let PreviousY = 0;
+
     let self = this;
 
     this.Coords = {
         GetX () {return X},
         GetY () {return Y},
+        GetPreviousX () {return PreviousX},
+        GetPreviousY () {return PreviousY},
         SetX (x) {X = x; return self},
         SetY (y) {Y = y; return self},
+        AddX (x) {X = PreviousX + x; return self},
+        AddY (y) {Y = PreviousY + y; return self},
+        SavePrevious () {
+            PreviousX = this.GetX();
+            PreviousY = this.GetY();
+        },
     };
 }
 
