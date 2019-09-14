8a79a1149486d840344d13b342c072128a211374
Tsv <tsv@serveon.ru>
2019-09-15 00:24:20 +0700

    try zoom

--- front/src/bubu/device/mouse.js
+++ front/src/bubu/device/mouse.js
@@ -66,11 +66,10 @@ function Mouse(config) {
 
         this.Move = (event) => {
 
-            // event = this.AssignCoordinates(event);
+            event = this.AssignCoordinates(event);
 
-
-            let newX = event.pageX - self.GetCanvasOffsetX();
-            let newY = event.pageY - self.GetCanvasOffsetY();
+            let newX = event.pageX - self.GetCanvasOffsetX() - self.GetSelectedItemOffsetX();
+            let newY = event.pageY - self.GetCanvasOffsetY() - self.GetSelectedItemOffsetY();
 
             // console.log('event.pageX', event.pageX, 'newX', newX, 'self.GetCanvasOffsetX()', self.GetSelectedItemOffsetX());
 
