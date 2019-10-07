e39c0e12d4eb6eef1734f9297676b74764d8479d
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2019-10-07 18:05:49 +0700

    try delete

--- front/src/bubu/elements/task.js
+++ front/src/bubu/elements/task.js
@@ -1,4 +1,5 @@
 import Types from "../attributes/types";
+import Delete from "../actions/delete";
 import {TYPES} from "../constants";
 import BasicSet from "../attributes/basic-set";
 
@@ -7,6 +8,7 @@ function Task(config) {
     BasicSet.apply(this, arguments);
 
     Types.apply(this, arguments);
+    Delete.apply(this, arguments);
     this.SetType(TYPES.task);
 
     return this;
