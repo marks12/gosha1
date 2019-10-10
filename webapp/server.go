ff832074cd44681297789d03c738fa2c3c2517f4
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2019-10-10 18:16:24 +0700

    need work

--- webapp/server.go
+++ webapp/server.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"gosha/settings"
 	"net/http"
+	"gosha/webapp/router"
 )
 
 func Run() {
 
 	fmt.Println("API server running :" + settings.ServerPort)
 	fmt.Println("Try to open: http://localhost:" + settings.ServerPort)
-	e := http.ListenAndServe("0.0.0.0:" + settings.ServerPort, Router())
+	e := http.ListenAndServe("0.0.0.0:" + settings.ServerPort, router.Router())
 
 	if e != nil {
 		fmt.Println("error running server: "  + e.Error())
