8dc05bf9b927802242fe3ba04ff0989c82e49722
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2020-02-19 16:12:21 +0700

    bf create just some methods

--- cmd/usual_template_route_entity.go
+++ cmd/usual_template_route_entity.go
@@ -13,16 +13,16 @@ const usualRouteEntityComment = `[ {Entity} ]`
 
 const usualRouteEntityFind = `router.HandleFunc(settings.{Entity}Route,         webapp.{Entity}Find).Methods("GET")`
 
-const usualRouteEntityCreate = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiCreate).Methods("POST")` + "\n" +
-							   `    router.HandleFunc(settings.{Entity}Route,         webapp.{Entity}Create).Methods("POST")`
+const usualRouteEntityCreate = `router.HandleFunc(settings.{Entity}Route,         webapp.{Entity}Create).Methods("POST")`
+const usualRouteEntityMultiCreate = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiCreate).Methods("POST")`
 
 const usualRouteEntityRead = `router.HandleFunc(settings.{Entity}Route+"/{id}", webapp.{Entity}Read).Methods("GET")`
 
-const usualRouteEntityUpdate = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiUpdate).Methods("PUT")` + "\n" +
-							   `    router.HandleFunc(settings.{Entity}Route+"/{id}", webapp.{Entity}Update).Methods("PUT")`
+const usualRouteEntityUpdate = `router.HandleFunc(settings.{Entity}Route+"/{id}", webapp.{Entity}Update).Methods("PUT")`
+const usualRouteEntityMultiUpdate = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiUpdate).Methods("PUT")`
 
-const usualRouteEntityDelete = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiDelete).Methods("DELETE")` + "\n" +
-							   `    router.HandleFunc(settings.{Entity}Route+"/{id}", webapp.{Entity}Delete).Methods("DELETE")`
+const usualRouteEntityDelete = `router.HandleFunc(settings.{Entity}Route+"/{id}", webapp.{Entity}Delete).Methods("DELETE")`
+const usualRouteEntityMultiDelete = `router.HandleFunc(settings.{Entity}Route+"/list", webapp.{Entity}MultiDelete).Methods("DELETE")`
 
 const usualRouteEntityFindOrCreate = `router.HandleFunc(settings.{Entity}Route,         webapp.{Entity}FindOrCreate).Methods("PUT")`
 
@@ -47,8 +47,10 @@ func GetUsualTemplateRouteEntity(c Crud) (res string) {
 
 	if c.IsCreate {
 		res += "\n    " + usualRouteEntityCreate
+		res += "\n    " + usualRouteEntityMultiCreate
 	} else {
 		res += "\n    //" + usualRouteEntityCreate
+		res += "\n    //" + usualRouteEntityMultiCreate
 	}
 
 	if c.IsRead {
@@ -59,14 +61,18 @@ func GetUsualTemplateRouteEntity(c Crud) (res string) {
 
 	if c.IsUpdate {
 		res += "\n    " + usualRouteEntityUpdate
+		res += "\n    " + usualRouteEntityMultiUpdate
 	} else {
 		res += "\n    //" + usualRouteEntityUpdate
+		res += "\n    //" + usualRouteEntityMultiUpdate
 	}
 
 	if c.IsDelete {
 		res += "\n    " + usualRouteEntityDelete
+		res += "\n    " + usualRouteEntityMultiDelete
 	} else {
 		res += "\n    //" + usualRouteEntityDelete
+		res += "\n    //" + usualRouteEntityMultiDelete
 	}
 
 	if c.IsFindOrCreate {
