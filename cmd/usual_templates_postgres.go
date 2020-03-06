0bb884740f4341dabcbd640a6e1573edebf30e31
Vladimir Tsv <tsarevnikov.vu@sibvaleo.com>
2020-03-06 18:06:42 +0700

    docker mysql added

--- cmd/usual_templates_postgres.go
+++ cmd/usual_templates_postgres.go
@@ -1,16 +1,31 @@
 package cmd
 
-const usualDockerCall = `#!/bin/bash
+const usualDockerCallPs = `#!/bin/bash
 docker run --rm --name pg-{ms-name} -e POSTGRES_DB={ms-name} -e POSTGRES_USER={ms-name} -e POSTGRES_PASSWORD={new-pass} -d -p 35432:5432 -v "$(pwd)/postgres:/var/lib/postgresql/data" postgres
 `
 
-var usualDockerCallontent =
+const usualDockerCallMy = `#!/bin/bash
+docker run --name my-{ms-name} -e MYSQL_ROOT_PASSWORD={new-pass} -d mariadb/server:10.3 --log-bin --binlog-format=MIXED
+`
+
+var usualDockerPs =
         assignMsName(
             assignPass(
-                usualDockerCall, dbPass))
+                usualDockerCallPs, dbPass))
 
 
-var usualTemplateDockerCallontent = template{
+var usualDockerMy =
+        assignMsName(
+            assignPass(
+                usualDockerCallMy, dbPass))
+
+
+var usualTemplateDockerPs = template{
     Path:    "./psql-docker.sh",
-    Content: usualDockerCallontent,
+    Content: usualDockerPs,
+}
+
+var usualTemplateDockerMy = template{
+    Path:    "./mysql-docker.sh",
+    Content: usualDockerMy,
 }
