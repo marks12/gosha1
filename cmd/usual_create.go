c5085591149242e317ee3e625bfa8492dc2dcd91
tsv <tsv@serveon.ru>
2020-03-28 19:36:51 +0700

    select dbtype

--- cmd/usual_create.go
+++ cmd/usual_create.go
@@ -1,11 +1,31 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"github.com/fatih/color"
 	"gopkg.in/abiosoft/ishell.v2"
 	"gosha/mode"
 	"os"
 )
+
+const dbTypeMysql = "mysql"
+const dbTypePostgres = "postgres"
+
+type DatabaseType struct {
+	IsMysql bool
+	IsPostgres bool
+	DbTypeName string
+}
+
+func (db *DatabaseType) Validate() {
+	db.IsPostgres = db.DbTypeName == dbTypePostgres
+	db.IsMysql = db.DbTypeName == dbTypeMysql
+}
+
+func (db *DatabaseType) IsValid() bool {
+	return db.IsPostgres != db.IsMysql
+}
 
 func UsualAppInit(c *ishell.Context) {
 
@@ -29,6 +49,12 @@ func UsualAppInit(c *ishell.Context) {
 
 	email, _ = getEmail(c)
 	password, _ = getPassword(c)
+	database, err := getDatabaseType(c)
+
+	if err != nil {
+		fmt.Println("Creating app error: " + err.Error())
+		os.Exit(1)
+	}
 
 	switch choice {
 
@@ -38,7 +64,7 @@ func UsualAppInit(c *ishell.Context) {
 			c.Println(green("Creating app structure"))
 		}
 
-		usualCreate(c, email, password)
+		usualCreate(c, email, password, database)
 		usualAuthAdd(c)
 
 		break
@@ -70,7 +96,33 @@ func getPassword(c *ishell.Context) (password string, err error) {
 	return
 }
 
-func usualCreate(c *ishell.Context, email, password string) {
+func getDatabaseType(c *ishell.Context) (dbtype DatabaseType, err error) {
+
+	var arguments RegularFind
+	arguments, err = GetOsArgument("dbType")
+
+	if err != nil || len(arguments.StringResult) < 1 {
+		return DatabaseType{}, errors.New("Invalid database. Only postgres, mysql supports " + dbTypeMysql + " " + dbTypePostgres + " ")
+	}
+
+	if isEx, _ := InArray(arguments.StringResult, []string{dbTypeMysql, dbTypePostgres}); isEx != true {
+		return DatabaseType{}, errors.New("Invalid database. Only postgres, mysql supports " + dbTypeMysql + " " + dbTypePostgres + " ")
+	}
+
+	dbtype = DatabaseType{
+		DbTypeName: arguments.StringResult,
+	}
+
+	dbtype.Validate()
+
+	if ! dbtype.IsValid() {
+		return DatabaseType{}, errors.New("Invalid database. Only postgres, mysql supports " + dbTypeMysql + " " + dbTypePostgres + " ")
+	}
+
+	return dbtype, nil
+}
+
+func usualCreate(c *ishell.Context, email, password string, databaseType DatabaseType) {
 
 	green := color.New(color.FgCyan).SprintFunc()
 	red := color.New(color.FgRed).SprintFunc()
@@ -87,6 +139,7 @@ func usualCreate(c *ishell.Context, email, password string) {
 		c)
 
 	//core
+	msTemplateCoreDb := getTemplateCoreDb(databaseType)
 	CreateFile(msTemplateCoreDb.Path, msTemplateCoreDb.Content, c)
 
 	//dbmodels
