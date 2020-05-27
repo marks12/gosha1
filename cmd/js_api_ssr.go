9023e6390b9839c69687831fb9714d589afc05ab
tsv <tsv@serveon.ru>
2020-05-27 14:23:19 +0700

    global filter parameters

--- cmd/js_api_ssr.go
+++ cmd/js_api_ssr.go
@@ -1,55 +1,99 @@
 package cmd
 
 const apiSSRContent = `
-
 import axios from "axios";
 
 function BackendApi() {
 
-  this.serverUrl = "";
+    this.serverUrl = "";
 
-  this.getRouteUrl = (url) => {
-    return this.serverUrl + url;
-  };
+    this.defaultParameters = {};
 
-  return {
-    create: (url, data, getParams, headerParams) => {
-      return axios.post(this.getRouteUrl(url),data, {params: getParams, headers: headerParams }).then((response) => {
-        return response.data;
-      }).catch( err => {
-          console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
-      });
-    },
-    find: (url, getParams, headerParams) => {
-      return axios.get(this.getRouteUrl(url), {params: getParams, headers: headerParams })
-        .then((response) => {
-          return response.data;
-      }).catch( err => {
-          console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
-      });
-    },
-    getServerUrl: () => {
-      return this.serverUrl;
-    },
-    remove: (url, getParams, data, headerParams) => {
-      return axios.delete(this.getRouteUrl(url), {data: data, params: getParams, headers: headerParams }).then((response) => {
-        return response.data;
-      }).catch( err => {
-          console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
-      });
-    },
-    setServerUrl: (url) => {
-      this.serverUrl = url;
-      return this;
-    },
-    update: (url, data, getParams, headerParams) => {
-      return axios.put(this.getRouteUrl(url), data, {params: getParams, headers: headerParams }).then((response) => {
-        return response.data;
-      }).catch( err => {
-          console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
-      });
-    },
-  };
+    this.getRouteUrl = (url) => {
+        return this.serverUrl + url;
+    };
+
+    let appendGlobalFilter = (filter) => {
+
+        for (let param in filter) {
+            if (filter.hasOwnProperty(param) && !filter[param] && this.defaultParameters.hasOwnProperty(param)) {
+                filter[param] = this.defaultParameters[param];
+            }
+        }
+
+        return filter;
+    }
+
+    return {
+        find: (url, getParams, headerParams) => {
+            getParams = appendGlobalFilter(getParams)
+            return axios.get(this.getRouteUrl(url), {params: getParams, headers: headerParams})
+                .then((response) => {
+                    return response.data;
+                }).catch(err => {
+                    console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
+                });
+        },
+        create: (url, data, getParams, headerParams) => {
+            getParams = appendGlobalFilter(getParams)
+            return axios.post(this.getRouteUrl(url), data, {
+                params: getParams,
+                headers: headerParams
+            }).then((response) => {
+                return response.data;
+            }).catch(err => {
+                console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
+            });
+        },
+        update: (url, data, getParams, headerParams) => {
+            getParams = appendGlobalFilter(getParams)
+            return axios.put(this.getRouteUrl(url), data, {
+                params: getParams,
+                headers: headerParams
+            }).then((response) => {
+                return response.data;
+            }).catch(err => {
+                console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
+            });
+        },
+        remove: (url, getParams, data, headerParams) => {
+            getParams = appendGlobalFilter(getParams)
+            return axios.delete(this.getRouteUrl(url), {
+                data: data,
+                params: getParams,
+                headers: headerParams
+            }).then((response) => {
+                return response.data;
+            }).catch(err => {
+                console.error(` + "`" + `Error in url request ${url}` + "`" + `, err)
+            });
+        },
+        getServerUrl: () => {
+            return this.serverUrl;
+        },
+        setServerUrl: (url) => {
+            this.serverUrl = url;
+            return this;
+        },
+        clearGlobalFilters: () => {
+            this.defaultParameters = {};
+            return this;
+        },
+        clearGlobalFilter: (name) => {
+            delete this.defaultParameters[name];
+            return this;
+        },
+        setGlobalFilterValue: (parameter, value) => {
+            this.defaultParameters[parameter] = value;
+            return this;
+        },
+        getGlobalFilters: () => {
+            return this.defaultParameters;
+        },
+        getGlobalFilter: (name) => {
+            return this.defaultParameters[name];
+        },
+    };
 }
 
 let apiSSR = new BackendApi();
