b8224c7beb20f86347f14b01292ac2b0aac8d400
Andrei Laptev <stcanabis@gmail.com>
2020-07-20 16:08:44 +0700

    add transform method in common.js

--- cmd/js_common.go
+++ cmd/js_common.go
@@ -18,6 +18,23 @@ export function findItemIndex(list, fn) {
     return index;
 }
 
+export function transformType(data, entity) {
 
+    if (Array.isArray && Array.isArray(data)) {
+        data.forEach(item => {
+            for (let key in item) {
+                if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])) {
+                    item[key] = Number(item[key])
+                }
+            }
+        });
+        return data;
+    }
 
-`
+    for (let key in data) {
+        if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])) {
+            data[key] = Number(data[key])
+        }
+    }
+    return data;
+}
--- cmd/js_store.go
+++ cmd/js_store.go
@@ -14,7 +14,7 @@ const UpdateOrCreateUrl = "/api/v1/{entity}"
 const storeTemplate = `
 import {{Entity}} from "../apiModel";
 import api from "../api";
-import {findItemIndex} from "../common";
+import {findItemIndex, transformType} from "../common";
 
 let findUrl = "` + FindUrl + `";
 let readUrl = "` + ReadUrl + `"; // + id
@@ -28,235 +28,223 @@ let findOrCreateUrl = "` + FindOrCreateUrl + `";
 let updateOrCreateUrl = "` + UpdateOrCreateUrl + `";
 
 const {entity} = {
-   actions: {
-       create{Entity}(context, {data, filter, header, noMutation}) {
-           let url = createUrl;
-        let entity = new {Entity}();
-           for (let key in data){
-               if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
-                   data[key] = Number(data[key])
-               }
-           }
-           if (Array.isArray && Array.isArray(data)) {
-           data.forEach(item=>{
-                  for (let key in item){
-                      if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
-                          item[key] = Number(item[key])
-                      }
-                  }
-               });
-               url = multiCreateUrl
-           }
+    actions: {
+		create{Entity}(context, {data, filter, header, noMutation}) {
+			
+			let url = createUrl;
+            if (Array.isArray && Array.isArray(data)) {
+                url = multiCreateUrl
+            }
+			transformType(data,new {Entity}());
+			
+            return api.create(url, data, filter, header)
+                .then(function(response) {
 
-           return api.create(url, data, filter, header)
-               .then(function(response) {
+					if(! noMutation) {
+	                    context.commit("set{Entity}", response.Model);
+					}
 
-              if(! noMutation) {
-                      context.commit("set{Entity}", response.Model);
-              }
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        delete{Entity}(context, {id, header, noMutation}) {
 
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       delete{Entity}(context, {id, header, noMutation}) {
+            let url;
+            let dataOrNull = null;
 
-           let url;
-           let dataOrNull = null;
+            if (Array.isArray && Array.isArray(id)) {
+                url = multiDeleteUrl;
+                dataOrNull = id.map(item => typeof item === "number" ? {Id: item} : item);
+            } else {
+                url = deleteUrl + id;
+            }
 
-           if (Array.isArray && Array.isArray(id)) {
-               url = multiDeleteUrl;
-               dataOrNull = id.map(item => typeof item === "number" ? {Id: item} : item);
-           } else {
-               url = deleteUrl + id;
-           }
+            return api.remove(url, header, dataOrNull)
+                .then(function(response) {
+					if(! noMutation) {
+	                    context.commit("clear{Entity}");
+					}
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        find{Entity}(context, {filter, header, isAppend, noMutation}) {
 
-           return api.remove(url, header, dataOrNull)
-               .then(function(response) {
-              if(! noMutation) {
-                      context.commit("clear{Entity}");
-              }
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       find{Entity}(context, {filter, header, isAppend, noMutation}) {
+            return api.find(findUrl, filter, header)
+                .then(function(response) {
 
-           return api.find(findUrl, filter, header)
-               .then(function(response) {
+					if(! noMutation) {
+						if (isAppend) {
+							context.commit("append{Entity}__List", response.List);
+						} else {
+							context.commit("set{Entity}__List", response.List);
+						}
+					}
 
-              if(! noMutation) {
-                 if (isAppend) {
-                    context.commit("append{Entity}__List", response.List);
-                 } else {
-                    context.commit("set{Entity}__List", response.List);
-                 }
-              }
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        load{Entity}(context, {id, filter, header, noMutation}) {
 
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       load{Entity}(context, {id, filter, header, noMutation}) {
+            return api.find(readUrl + id, filter, header)
+                .then(function(response) {
 
-           return api.find(readUrl + id, filter, header)
-               .then(function(response) {
+					if(! noMutation) {
+	                    context.commit("set{Entity}", response.Model);
+					}
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        update{Entity}(context, {id, data, filter, header, noMutation}) {
+            let url = updateUrl + id;
+			let entity = new {Entity}();
+			for (let key in data){
+				if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
+					data[key] = Number(data[key])
+				}
+			}
+            if (Array.isArray && Array.isArray(data)) {
+				data.forEach(item=>{
+					for (let key in item){
+						if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
+							item[key] = Number(item[key])
+						}
+					}
+				});
+                url = multiUpdateUrl
+            }
 
-              if(! noMutation) {
-                      context.commit("set{Entity}", response.Model);
-              }
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       update{Entity}(context, {id, data, filter, header, noMutation}) {
-           let url = updateUrl + id;
-        let entity = new {Entity}();
-           for (let key in data){
-               if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
-                   data[key] = Number(data[key])
-               }
-           }
-           if (Array.isArray && Array.isArray(data)) {
-           data.forEach(item=>{
-                  for (let key in item){
-                      if (entity.hasOwnProperty(key) && Number.isInteger(entity[key])){
-                          item[key] = Number(item[key])
-                      }
-                  }
-               });
-               url = multiUpdateUrl
-           }
+            return api.update(url, data, filter, header)
+                .then(function(response) {
+					if(! noMutation) {
+	                    context.commit("set{Entity}", response.Model);
+					}
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        findOrCreate{Entity}(context, {id, data, filter, header, noMutation}) {
 
-           return api.update(url, data, filter, header)
-               .then(function(response) {
-              if(! noMutation) {
-                      context.commit("set{Entity}", response.Model);
-              }
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       findOrCreate{Entity}(context, {id, data, filter, header, noMutation}) {
+            return api.update(findOrCreateUrl, data, filter, header)
+                .then(function(response) {
 
-           return api.update(findOrCreateUrl, data, filter, header)
-               .then(function(response) {
+					if(! noMutation) {
+	                    context.commit("set{Entity}", response.Model);
+					}
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    throw(err);
+                });
+        },
+        clearList{Entity}(context) {
+            context.commit("clearList{Entity}");
+        },
+        clear{Entity}(context) {
+            context.commit("clear{Entity}");
+        },
+    },
+    getters: {
+        get{Entity}: (state) => {
+            return state.{Entity};
+        },
+        get{Entity}ById: state => id => {
+            return state.{Entity}__List.find(item => item.Id === id);
+        },
+        getList{Entity}: (state) => {
+            return state.{Entity}__List;
+        },
+        getRoute__{Entity}: state => action => {
+            return state.{Entity}__Routes[action];
+        },
+        getRoutes__{Entity}: state => {
+            return state.{Entity}__Routes;
+        },
+    },
+    mutations: {
+        set{Entity}(state, data) {
+            state.{Entity} = data;
+        },
+        set{Entity}__List(state, data) {
+            state.{Entity}__List = data || [];
+        },
+        append{Entity}__List(state, data) {
 
-              if(! noMutation) {
-                      context.commit("set{Entity}", response.Model);
-              }
-                   return response;
-               })
-               .catch(function(err) {
-                   console.error(err);
-                   throw(err);
-               });
-       },
-       clearList{Entity}(context) {
-           context.commit("clearList{Entity}");
-       },
-       clear{Entity}(context) {
-           context.commit("clear{Entity}");
-       },
-   },
-   getters: {
-       get{Entity}: (state) => {
-           return state.{Entity};
-       },
-       get{Entity}ById: state => id => {
-           return state.{Entity}__List.find(item => item.Id === id);
-       },
-       getList{Entity}: (state) => {
-           return state.{Entity}__List;
-       },
-       getRoute__{Entity}: state => action => {
-           return state.{Entity}__Routes[action];
-       },
-       getRoutes__{Entity}: state => {
-           return state.{Entity}__Routes;
-       },
-   },
-   mutations: {
-       set{Entity}(state, data) {
-           state.{Entity} = data;
-       },
-       set{Entity}__List(state, data) {
-           state.{Entity}__List = data || [];
-       },
-       append{Entity}__List(state, data) {
+            if (! state.{Entity}__List) {
+                state.{Entity}__List = [];
+            }
 
-           if (! state.{Entity}__List) {
-               state.{Entity}__List = [];
-           }
+            state.{Entity}__List = state.{Entity}__List.concat(data);
+        },
+        clear{Entity}(state) {
+            state.{Entity} = new {Entity}();
+        },
+        clearList{Entity}(state) {
+            state.{Entity}__List = [];
+        },
+		update{Entity}ById(state, data) {
+    		let index = findItemIndex(state.{Entity}__List, function(item) {
+	        	return item.Id === data.Id;
+	    	});
+	    
+	    	if (index || index === 0) {
+		        state.{Entity}__List.splice(index, 1, data);
+    		}
+		},
+		delete{Entity}FromList(state, id) {
+		    let index = findItemIndex(state.{Entity}__List, function(item) {
+		        return item.Id === id;
+		    });
+		    
+		    if (index || index === 0) {
+		        state.{Entity}__List.splice(index, 1);
+		    }
+		},
+		add{Entity}ItemToList(state, item) {
 
-           state.{Entity}__List = state.{Entity}__List.concat(data);
-       },
-       clear{Entity}(state) {
-           state.{Entity} = new {Entity}();
-       },
-       clearList{Entity}(state) {
-           state.{Entity}__List = [];
-       },
-     update{Entity}ById(state, data) {
-         let index = findItemIndex(state.{Entity}__List, function(item) {
-             return item.Id === data.Id;
-         });
-     
-         if (index || index === 0) {
-             state.{Entity}__List.splice(index, 1, data);
-         }
-     },
-     delete{Entity}FromList(state, id) {
-         let index = findItemIndex(state.{Entity}__List, function(item) {
-             return item.Id === id;
-         });
-        
-         if (index || index === 0) {
-             state.{Entity}__List.splice(index, 1);
-         }
-     },
-     add{Entity}ItemToList(state, item) {
+			if (state.{Entity}__List === null) {
+				state.{Entity}__List = [];
+			}
 
-        if (state.{Entity}__List === null) {
-           state.{Entity}__List = [];
-        }
-
-         state.{Entity}__List.push(item);
-     },
-   },
-   state: {
-       {Entity}: new {Entity}(),
-       {Entity}__List: [],
-       {Entity}__Routes: {
-           find: findUrl,
-           read: readUrl,
-           create: createUrl,
-           multiCreate: multiCreateUrl,
-           update: updateUrl,
-           multiUpdate: multiUpdateUrl,
-           delete: deleteUrl,
-           multiDelete: multiDeleteUrl,
-           findOrCreate: findOrCreateUrl,
-           updateOrCreate: updateOrCreateUrl,
-       },
-   },
+		    state.{Entity}__List.push(item);
+		},
+    },
+    state: {
+        {Entity}: new {Entity}(),
+        {Entity}__List: [],
+        {Entity}__Routes: {
+            find: findUrl,
+            read: readUrl,
+            create: createUrl,
+            multiCreate: multiCreateUrl,
+            update: updateUrl,
+            multiUpdate: multiUpdateUrl,
+            delete: deleteUrl,
+            multiDelete: multiDeleteUrl,
+            findOrCreate: findOrCreateUrl,
+            updateOrCreate: updateOrCreateUrl,
+        },
+    },
 };
 
 export default {entity};
 `
-
