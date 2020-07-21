64528d8ba2d30b76ac8ac1aea7815a4a805faa85
BlayD91 <geymer91@gmail.com>
2020-07-21 22:04:16 +0700

    [WIP] Add auto update Add current release tag in app

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-    "gosha/cmd"
-    "gosha/mode"
-    "gosha/webapp"
-    "os"
+	"fmt"
+	"gosha/cmd"
+	"gosha/mode"
+	"gosha/settings"
+	"gosha/updater"
+	"gosha/webapp"
+	"log"
+	"os"
+	"syscall"
 )
 
 func main() {
+	args := []string{}
+	for _, arg := range os.Args {
+		if arg == "childProcess" {
+			parent := syscall.Getppid()
+			log.Printf("main: Killing parent pid: %v", parent)
+			syscall.Kill(parent, syscall.SIGTERM)
+		} else {
+			args = append(args, arg)
+		}
+	}
+	os.Args = args
 
-    if len(os.Args) > 1 {
-        cmd.Run()
-        return
-    }
+	fmt.Println("Current version:", settings.CurrentReleaseTag)
 
-    mode.SetNonInteractiveMode()
-    webapp.Run()
+	isRestart, err := updater.MakeUpdate()
+	if err != nil {
+		fmt.Println("Error in AutoUpdate:", err.Error())
+	}
+
+	if isRestart {
+		binPath, _ := os.Executable()
+		os.Args = append(os.Args, "childProcess")
+		if err = syscall.Exec(binPath, os.Args, os.Environ()); err != nil {
+			panic(err)
+		}
+	}
+
+	if len(os.Args) > 1 {
+		cmd.Run()
+		return
+	}
+
+	mode.SetNonInteractiveMode()
+	webapp.Run()
 }
